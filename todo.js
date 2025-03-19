2da9ab883890bbab140dbed5c893ced728065a41
reelohara <edwardevandelavin@gmail.com>
2025-03-19 16:56:28 +0800

    created list values and added display function

--- todo.js
+++ todo.js
@@ -1,2 +1,20 @@
 // how to display the todos list on page load
 // how to add todo on button click
+const todoArr = ["Wash the dishes", "Take out the trash", "Walk the dog"]
+const todoList= document.querySelector("#todos")
+const formEL = document.querySelector("#todoForm")
+
+formEL.addEventListener("submit", newTodo)
+function display() {
+  for (let i = 0; i < todoArr.length; i++) {
+    const todoItem = document.createElement("li") 
+    todoItem.innerText += todoArr[i] 
+    todoList.appendChild(todoItem)
+  }
+}
+
+function newTodo() {
+  
+}
+
+display()
