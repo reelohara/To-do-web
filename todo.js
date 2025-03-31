fd79e3865bb6f5c27e0b05a820f718ebf2b33bb9
reelohara <edwardevandelavin@gmail.com>
2025-03-31 15:29:01 +0800

    Added save functionality that saves to local Storage

--- todo.js
+++ todo.js
@@ -1,23 +1,27 @@
 // how to display the todos list on page load
 // how to add todo on button click
-let todoArr = ["Wash the dishes", "Take out the trash", "Walk the dog"]
+
+const todoArr = retrieveData()
 const todoList= document.querySelector("#todos")
 const formEl = document.querySelector("#todoForm")
 const buttonEl = document.querySelector("button[value = addTodo]")
 const inputEl = document.querySelector("input[name = newTodo]")
 
+display()
+
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(formEl) 
-  for (data of formData){
-    todoArr = [ ]
-    todoArr.push(data[1])
-    inputEl.value = ""
-  }
+  const formData = new FormData(formEl)
+  const name = formData.get('newTodo')
+
+  todoArr.push(name)
+  inputEl.value = ""
+  saveData()
   display()
 })
-function display() {
 
+function display() {
+  todoList.innerHTML = ""
   for (let i = 0; i < todoArr.length; i++) {
     const todoItem = document.createElement("li") 
     todoItem.innerText += todoArr[i] 
@@ -25,7 +29,11 @@ function display() {
   }
 }
 
+function saveData() {
+  localStorage.setItem("data", JSON.stringify(todoArr))
+}
 
-function newTodo() {
-  
+function retrieveData() {
+  const values = JSON.parse(localStorage.getItem("data"))
+  return values;
 }
